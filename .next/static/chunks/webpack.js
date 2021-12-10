8fafb2a5b14b25e8ca9726bd8502660a8812ced1
ArnaVala <arnavala@gmail.com>
2021-12-10 11:35:11 +0000

    fix

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "03061c98429d0f33"; }
+/******/ 		__webpack_require__.h = function() { return "d1ea97e682c0960a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
